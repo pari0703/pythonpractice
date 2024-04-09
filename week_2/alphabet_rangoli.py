fb542a8c1f402b650fdbd20e7196f3daba0fa15e
pari0703 <pariagarwal0703@gmail.com>
2024-04-09 11:30:19 +0530

    this is my 35th commit

---
+++ week_2/alphabet_rangoli.py
@@ -0,0 +1,22 @@
+def rangoli(size):
+    import string
+    alphabet = string.ascii_lowercase
+    
+    # Create the top part of the rangoli
+    rows = []
+    for i in range(size):
+        row = '-'.join(alphabet[size-1:i:-1] + alphabet[i:size])
+        rows.append(row.center(4*size - 3, '-'))
+    
+    # Create the bottom part of the rangoli by reversing the top part
+    result = rows[::-1] + rows[1:]
+    
+    # Joining the rows with newline character
+    return '\n'.join(result)
+def print_rangoli(size):
+    print(rangoli(size))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
