cbd8e636b381e8dcb9bfddd3efb6235f83fed575
pari0703 <pariagarwal0703@gmail.com>
2024-04-15 12:37:34 +0530

    this is my 69th commit

---
+++ week_2/nearest_grade.py
@@ -0,0 +1,27 @@
+def gradingStudents(grades):
+    rounded_grades = []
+    for grade in grades:
+        if grade < 38:
+            rounded_grades.append(grade)
+        else:
+            next_multiple_of_5 = (grade // 5 + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                rounded_grades.append(next_multiple_of_5)
+            else:
+                rounded_grades.append(grade)
+    return rounded_grades
+if __name__ == '__main__':
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    print('\n'.join(map(str, result)))
+    print('\n')
+
